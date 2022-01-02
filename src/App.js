c100d14461326eee7b1681321b85b88a6a0ed469
zeronedev <ok7375@gmail.com>
2022-01-02 19:57:26 +0900

    #7.2 Coin Tracker

--- src/App.js
+++ src/App.js
@@ -1,7 +1,38 @@
+import { useState } from "react";
+
 function App() {
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
+  const onChange = (event) => setToDo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (toDo === "") {
+      return;
+    }
+    setToDos((currentArray) => [toDo, ...currentArray]);
+    setToDo("");
+  };
+  // console.log(toDos);
+  // console.log(toDos.map((item, index) => <li key={index}>{item}</li>));
+
   return (
     <div>
-      <h1>Welcome React</h1>
+      <h1>My To Dos ({toDos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          type="text"
+          placeholder="write your to do..."
+          value={toDo}
+          onChange={onChange}
+        />
+        <button>Add To Do</button>
+      </form>
+      <hr />
+      <ul>
+        {toDos.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import Coin from "./Coin";
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <Coin />
   </React.StrictMode>,
   document.getElementById("root")
 );
